3cee363f606046260180c92960c3a48c155b1639
AzrinHakimi00 <153930758+AzrinHakimi00@users.noreply.github.com>
2023-12-28 19:57:38 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,33 @@
-// Function to load JSON data from a file
+let noQ = 0;
+let nextQuestion = document.querySelector('.next-question');
+let isAnswered = false; // Move isAnswered outside of loadJsonData function
+
 async function loadJsonData() {
     try {
+        var isAnswered = false;
         // Fetch the JSON data from the file
         const response = await fetch('data.json');
 
         // Parse the JSON data
         const jsonData = await response.json();
 
+        // Clear the color of the selected answer from the previous question
+        clearAnswerColors();
+
         // Update the DOM elements with the loaded data
-        updateElements(jsonData);
+        updateElements(jsonData[noQ], isAnswered);
     } catch (error) {
         console.error('Error loading JSON data:', error);
     }
+}
+
+
+// Function to clear the color of the selected answer from the previous question
+function clearAnswerColors() {
+    let answerElements = document.querySelectorAll('.answer div');
+    answerElements.forEach(answer => {
+        answer.style.backgroundColor = '';
+    });
 }
 
 // Function to update DOM elements with JSON data
@@ -34,25 +50,34 @@ function updateElements(data) {
 
     // Adding click event listeners to answer elements
     ans1.addEventListener('click', function() {
-        checkAnswer(data.answers.ans1, data.correctAnswer, data.trivia);
-        ans1.style.backgroundColor = "#76FF03";
+        
+        checkAnswer(data.answers.ans1, data.correctAnswer, data.trivia, ans1);
+        
     });
 
     ans2.addEventListener('click', function() {
-        checkAnswer(data.answers.ans2, data.correctAnswer, data.trivia);
+       
+        checkAnswer(data.answers.ans2, data.correctAnswer, data.trivia, ans2);
+        
     });
 
     ans3.addEventListener('click', function() {
-        checkAnswer(data.answers.ans3, data.correctAnswer, data.trivia);
-    });
+      
+        checkAnswer(data.answers.ans3, data.correctAnswer, data.trivia, ans3);
+        });
 
     ans4.addEventListener('click', function() {
-        checkAnswer(data.answers.ans4, data.correctAnswer, data.trivia);
+       
+        checkAnswer(data.answers.ans4, data.correctAnswer, data.trivia, ans4);
+        
     });
+
+    let triviaElement = document.querySelector('.trivia');
+    triviaElement.textContent = '';
 }
 
 // Function to check the selected answer
-function checkAnswer(selectedAnswer, correctAnswer, trivia) {
+function checkAnswer(selectedAnswer, correctAnswer, trivia, answer) {
     // Replace this with your logic to check if the selected answer is correct
     let row = document.querySelector('.right-or-wrong');
     let triviaElement = document.querySelector('.trivia');
@@ -60,13 +85,31 @@ function checkAnswer(selectedAnswer, correctAnswer, trivia) {
     if (selectedAnswer === correctAnswer) {
         row.textContent = "Correct!";
         row.style.color = "#76FF03";
-        
+        answer.style.backgroundColor = "#76FF03";
     } else {
-        row.textContent = "Wrong!";
+        row.textContent = "Wrong! The correct answer is " + correctAnswer;
         row.style.color = "#E53935";
+        answer.style.backgroundColor = "#E53935";
     }
     triviaElement.textContent = trivia;
+
+    nextQuestion.style.display = 'block';
+
+
 }
+
+// Add event listener for the "Next Question" button outside the function
+nextQuestion.addEventListener('click', function() {
+    let triviaElement = document.querySelector('.trivia');
+    triviaElement.textContent = "";
+    let row = document.querySelector('.right-or-wrong');
+    row.textContent = "";
+    noQ++;
+    loadJsonData();
+    if(noQ == 10) {
+        alert('You have answered all the question')
+    }
+});
 
 // Load JSON data when the script is executed
 loadJsonData();
